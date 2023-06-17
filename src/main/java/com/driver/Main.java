ce8e08068b8450f846c82c29d3d775a2e8384e09
ritikbarmecha <ritikbarmecha@gmail.com>
2023-06-18 01:00:03 +0530

    ss

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,22 @@
 package com.driver;
-
+import java.lang.*;
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        BankAccount bankAccount = new BankAccount("Aniruddha", 5000, 2000);
+        System.out.println(bankAccount.getBalance());
+        System.out.println(bankAccount.getName());
+        try {
+            System.out.println(bankAccount.generateAccountNumber(100, 9));
+        }catch (Exception exception){
+            System.out.println(exception.toString());
+        }
+//        System.out.println("Bank Account Done");
+        CurrentAccount currentAccount = new CurrentAccount("Arindam", 50000, "AAABBCAACC");
+        try {
+            currentAccount.validateLicenseId();
+            System.out.println(currentAccount.tradeLicenseId);
+        }catch (Exception exception) {
+            System.out.println(exception.toString());
+        }
     }
 }
